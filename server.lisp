0551e75498db982ff5a41caddb5a8607f285d5c9
Ilya Khaprov <dead.trickster@gmail.com>
2014-11-28 02:34:30 -0800

    soft shutdown attemp #1

--- server.lisp
+++ server.lisp
@@ -16,6 +16,10 @@
             selves (just used as a set for now).")
    (resources :initform (make-hash-table :test 'equal) :reader server-resources
               :documentation "Collection of resource which handled by this server")
+   (shutting-down-p :initform nil
+                    :accessor shutting-down-p
+                    :documentation "If t server enters shutting-down mode.
+Stop accepting new connections and all incoming messages. After last outgoing message is written server killed.")
    (control-fd :initform nil :reader server-control-fd)
    (control-mailbox :initform nil :reader server-control-mailbox)
    (execute-in-server-lambda :initform nil :reader execute-in-server-lambda
@@ -48,31 +52,34 @@ connections and has a bunch of client instances that it controls."))
 (defun make-listener-handler (server socket)
   (lambda (fd event exception)
      (declare (ignore fd event exception))
-     (let* ((client-socket (accept-connection socket :wait t))
-            (client (when client-socket
-                      (make-instance (server-client-class server)
-                                     :server server
-                                     :%host (remote-host client-socket)
-                                     :host (address-to-string
-                                            (remote-host client-socket))
-                                     :port (remote-port client-socket)
-                                     :server-hook (slot-value server 'execute-in-server-lambda)
-                                     :socket client-socket))))
-       (when client
-         (log:debug "got client connection from ~s ~s~%" (client-host client)
-             (client-port client))
-         (log:debug "client count = ~s/~s~%" (server-client-count server) *max-clients*)
-         (cond
-           ((and *max-clients* (> (server-client-count server) *max-clients*))
-            ;; too many clients, send a server busy response and close connection
-            (log:info "Too many clients ~s:~s, sending busy response" (server-client-count server) *max-clients*)
-            (client-disconnect client :read t)
-            (client-enqueue-write client *server-busy-message*)
-            (client-enqueue-write client :close))
-           (t
-            ;; otherwise handle normally
-            (setf (gethash client (server-clients server)) client)
-            (add-reader-to-client client)))))))
+     (if (shutting-down-p server)
+         ;; stop accepting new connections first
+         (iolib:remove-fd-handlers (server-event-base server) (iolib:socket-os-fd socket))
+         (let* ((client-socket (accept-connection socket :wait t))
+                (client (when client-socket
+                          (make-instance (server-client-class server)
+                                         :server server
+                                         :%host (remote-host client-socket)
+                                         :host (address-to-string
+                                                (remote-host client-socket))
+                                         :port (remote-port client-socket)
+                                         :server-hook (slot-value server 'execute-in-server-lambda)
+                                         :socket client-socket))))
+           (when client
+             (log:debug "got client connection from ~s ~s~%" (client-host client)
+                        (client-port client))
+             (log:debug "client count = ~s/~s~%" (server-client-count server) *max-clients*)
+             (cond
+               ((and *max-clients* (> (server-client-count server) *max-clients*))
+                ;; too many clients, send a server busy response and close connection
+                (log:info "Too many clients ~s:~s, sending busy response" (server-client-count server) *max-clients*)
+                (client-disconnect client :read t)
+                (client-enqueue-write client *server-busy-message*)
+                (client-enqueue-write client :close))
+               (t
+                ;; otherwise handle normally
+                (setf (gethash client (server-clients server)) client)
+                (add-reader-to-client client))))))))
 
 (defun run-server% (server)
   (let ((event-base (server-event-base server))
@@ -90,33 +97,34 @@ connections and has a bunch of client instances that it controls."))
                                          :backlog 5
                                          :reuse-address t
                                          #++ :no-delay)
-           (iolib:set-io-handler event-base
-                                 control-fd
-                                 :read (lambda (fd e ex)
-                                          (declare (ignorable fd e ex))
-                                          (log:debug "Got lambda to execute on server thread ~a" (eventfd.read control-fd))
-                                          (loop for m = (dequeue control-mailbox)
-                                                while m
-                                                do (funcall m))))
-           (iolib:set-io-handler event-base
-                                 (iolib:socket-os-fd socket)
-                                 :read (let ((true-handler (make-listener-handler
-                                                            server
-                                                            socket)))
-                                         (lambda (&rest rest)
-                                            (log:debug "There is something to read on fd ~A" (first rest))
-                                            (apply true-handler rest))))
-           (handler-case
-               (event-dispatch event-base)
-             ;; ... handle errors
-             ))
-      (loop :for v :in (server-list-clients server)
-            :do
-               (log:debug "cleanup up dropping client ~s" v)
-               (client-enqueue-read v (list v :dropped))
-               (client-disconnect v :abort t))
-      (close event-base)
-      (close-eventfd control-fd))))
+           (let ((true-handler (make-listener-handler
+                                server
+                                socket)))
+             (iolib:set-io-handler event-base
+                                   control-fd
+                                   :read (lambda (fd e ex)
+                                            (declare (ignorable fd e ex))
+                                            (log:debug "Got lambda to execute on server thread ~a" (eventfd.read control-fd))
+                                            (loop for m = (dequeue control-mailbox)
+                                                  while m
+                                                  do (funcall m))))
+             (iolib:set-io-handler event-base
+                                   (iolib:socket-os-fd socket)
+                                   :read
+                                   (lambda (&rest rest)
+                                      (log:debug "There is something to read on fd ~A" (first rest))
+                                      (apply true-handler rest)))
+             (handler-case
+                 (event-dispatch event-base)
+               ;; ... handle errors
+               ))
+           (loop :for v :in (server-list-clients server)
+                 :do
+                    (log:debug "cleanup up dropping client ~s" v)
+                    (client-enqueue-read v (list v :dropped))
+                    (client-disconnect v :abort t))
+           (close event-base)
+           (close-eventfd control-fd)))))
 
 (defun setup-execute-in-server-lambda (server)
   (let* ((control-mailbox (make-queue :name (concatenate 'string (server-name server) " server-control")))
@@ -148,7 +156,7 @@ spawned off a new thread for each connection.  As such, most of the
 processing is done on the Server Thread, though most user functions
 are thread-safe.
 
-"  
+"
   (log:info "CLWS ~a" (server-name server))
   (setup-execute-in-server-lambda server)
   (log:info "Starting server resources")
@@ -164,15 +172,55 @@ are thread-safe.
   "Calls RUN-SERVER with given SERVER in separate thread.
 Return SERVER"
   (bt:make-thread
-   (lambda ()      
+   (lambda ()
       (run-server server))
    :name (server-name server))
   server)
 
-
-(defun stop-server (server)
-  (log:info "Killing resources~%")
+(defun careless-stop (server)
+  (log:info "~a: Killing resources~%" (server-name server))
   (kill-all-server-resource-listeners server)
-  (log:info "Stopping Server Thread~%")
+  (log:info "~a: Stopping Server Thread~%" (server-name server))
+  (funcall (execute-in-server-lambda server) (lambda () (sb-thread:abort-thread))))
+
+(defun stop-new-messages (server)
+  (log:info "~a: Stop accepting new connections" (server-name server))
   (funcall (execute-in-server-lambda server) (lambda ()
-                                                (sb-thread:abort-thread))))
+                                                (setf (shutting-down-p server) t)
+                                                (with-hash-table-iterator (next-client (server-clients server))
+                                                  (loop
+                                                    (multiple-value-bind (next? key client) (next-client)
+                                                      (declare (ignorable key))
+                                                      (unless next?
+                                                        (return))
+                                                            ;disable read
+                                                      (setf (client-read-closed client) t)
+                                                      (client-disable-handler client :read t)))))))
+
+(defun stop-exsiting-clients (server close-code)  
+  (log:info "~a: Stop reading new incoming messages and enqueue close frame for all clients" (server-name server))
+  (funcall (execute-in-server-lambda server) (lambda ()
+                                                (with-hash-table-iterator (next-client (server-clients server))
+                                                  (loop
+                                                    (multiple-value-bind (next? key client) (next-client)
+                                                      (declare (ignorable key))
+                                                      (unless next?
+                                                        (return))
+                                                      ;;enqueue close
+                                                      (write-to-client-close client :code close-code))))))
+  (log:info "~a: Waiting until all clients closed" (server-name server))
+  (loop
+    (when (= 0 (hash-table-count (server-clients server)))
+      (return))))
+
+(defun stop-server (server &key soft stop-new-messages-cb close-exsiting-clients-cb (close-code 3502))
+  (if soft
+      (progn
+        (stop-new-messages server)
+        (when stop-new-messages-cb
+          (funcall stop-new-messages-cb server))
+        (stop-exsiting-clients server close-code)
+        (when close-exsiting-clients-cb
+          (funcall close-exsiting-clients-cb server))
+        (careless-stop server))
+      (careless-stop server)))
